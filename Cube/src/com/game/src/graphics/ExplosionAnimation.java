0fae8634438868932f2500729ba55b61d053af6d
HikingCarrot7 <47760015+HikingCarrot7@users.noreply.github.com>
2019-07-20 22:20:44 -0500

    20/07/2019

--- Cube/src/com/game/src/graphics/ExplosionAnimation.java
+++ Cube/src/com/game/src/graphics/ExplosionAnimation.java
@@ -7,61 +7,75 @@ import java.util.Random;
 
 public class ExplosionAnimation
 {
-	private ArrayList<Particles> particulas;
-	
-	public ExplosionAnimation(float x, float y) 
-	{
-		particulas = new ArrayList<>();
-		
-		for (int i = 0; i < 30; i++) 
-			particulas.add(new Particles(x , y));
-	}
 
-	public void tick() 
-	{
-		for (Particles P : particulas) 
-			P.tick();	
-	}
+    private ArrayList<Particles> particulas;
 
-	public void render(Graphics2D g) 
-	{	
-		for (Particles P : particulas) 
-			P.render(g);
-	}
-	
-	private class Particles
-	{
-		private Random rand;
-		
-		private float x, y, velX, velY;
-		
-		public Particles(float x, float y)
-		{
-			rand = new Random();
-			
-			this.x = x;
-			this.y = y;
-			
-			velX = rand.nextInt(10) - 5;
-			velY = rand.nextInt(10) - 5;
-			
-			if(velX == 0) velX = 5;
-			if(velY == 0) velY = 3;
-			
-		}
+    public ExplosionAnimation(float x, float y)
+    {
+        particulas = new ArrayList<>();
 
-		public void tick()
-		{
-			x += velX;
-			y += velY;
-		}
+        for (int i = 0; i < 30; i++)
+        {
+            particulas.add(new Particles(x, y));
+        }
+    }
 
-		public void render(Graphics2D g) 
-		{
-			g.setColor(Color.green);
-			g.fillRect((int) x, (int) y, 5, 5);
-		}
+    public void tick()
+    {
+        for (Particles P : particulas)
+        {
+            P.tick();
+        }
+    }
 
-	}
+    public void render(Graphics2D g)
+    {
+        for (Particles P : particulas)
+        {
+            P.render(g);
+        }
+    }
+
+    private class Particles
+    {
+
+        private Random rand;
+
+        private float x, y, velX, velY;
+
+        public Particles(float x, float y)
+        {
+            rand = new Random();
+
+            this.x = x;
+            this.y = y;
+
+            velX = rand.nextInt(10) - 5;
+            velY = rand.nextInt(10) - 5;
+
+            if (velX == 0)
+            {
+                velX = 5;
+            }
+            if (velY == 0)
+            {
+                velY = 3;
+            }
+
+        }
+
+        public void tick()
+        {
+            x += velX;
+            y += velY;
+        }
+
+        public void render(Graphics2D g)
+        {
+            g.setColor(Color.green);
+            g.fillRect((int) x, (int) y, 5, 5);
+        }
+
+    }
 
 }
--- Cube/src/com/game/src/graphics/HUD.java
+++ Cube/src/com/game/src/graphics/HUD.java
@@ -7,99 +7,100 @@ import java.awt.Graphics;
 import com.game.src.main.Game;
 import com.game.src.objects.Handler;
 
-public class HUD 
+public class HUD
 {
-	public static int HEALTH = 100;
-	public int bounds = 0;
-	private boolean LevelAnimation = false;
-	
-	private float y = - 30, velY = 8f;
-	
-	private int score = 0, level = 1;
 
-	private Game game;
-	private Handler handler;
-	
-	public HUD(Game game, Handler handler) 
-	{
-		this.game = game;
-		this.handler = handler;
-	}
-	
-	public void tick() 
-	{
-		Game.clamp(HEALTH, 100 + bounds, 0);
-		
-		score++;
-		
-		if(LevelAnimation) 
-		{
-			y += velY;
-			
-			if(y >= Game.ALTO/2) 
-			{
-				LevelAnimation = false;
-				
-				y = -30;
-			}
-		}
-			
-		if(HEALTH <= 0) 
-		{
-			Game.gameState = Game.STATE.End;
-			
-			game.addExplosion(new ExplosionAnimation(handler.getObjects().get(0).getX(), handler.getObjects().get(0).getY()));
-		}
-	}
-	
-	public void render(Graphics g) 
-	{
-		g.setColor(Color.gray);
-		g.fillRect(5, 5, 200 + bounds, 32);
-		
-		g.setColor(Color.getHSBColor( (1f * HEALTH) / 360, 1f, 1f));
-		g.fillRect(5, 5, HEALTH * 2, 32);
-		
-		g.setColor(Color.white);
-		g.drawRect(5, 5, 200 + bounds, 32);
-		
-		g.setFont(new Font("serif", Font.BOLD, 15));
-		g.drawString("Score: " + score, 5, 55);
-		
-		g.drawString("Level: " + level, 5, 70);
-		
-		g.drawString(Game.difficulty ? "Difficulty: HARD" : "Difficulty: NORMAL", 5, 85);
-		
-		g.drawString("Space for shop", 5, 100);
-		
-		g.setFont(new Font("serif", Font.BOLD, 50));
-		g.drawString(getLevel() % 10 == 0 ? "BOSS FIGHT!!" : "Level: " + level + "!", getLevel() % 10 == 0 ? Game.ANCHO/2 - 180 : Game.ANCHO/2 - 100, (int) y);
-		
-	}
+    public static int HEALTH = 100;
+    public int bounds = 0;
+    private boolean LevelAnimation = false;
 
-	public int getScore()
-	{
-		return score;
-	}
+    private float y = - 30, velY = 8f;
 
-	public void setScore(int score) 
-	{
-		this.score = score;
-	}
+    private int score = 0, level = 1;
 
-	public int getLevel() 
-	{
-		return level;
-	}
+    private Game game;
+    private Handler handler;
 
-	public void setLevel(int level) 
-	{
-		this.level = level;
-	}
-	
-	public void setLevelAnimation(boolean levelAnimation) 
-	{
-		LevelAnimation = levelAnimation;
-	}
-	
+    public HUD(Game game, Handler handler)
+    {
+        this.game = game;
+        this.handler = handler;
+    }
+
+    public void tick()
+    {
+        Game.clamp(HEALTH, 100 + bounds, 0);
+
+        score++;
+
+        if (LevelAnimation)
+        {
+            y += velY;
+
+            if (y >= Game.ALTO / 2)
+            {
+                LevelAnimation = false;
+
+                y = -30;
+            }
+        }
+
+        if (HEALTH <= 0)
+        {
+            Game.gameState = Game.STATE.End;
+
+            game.addExplosion(new ExplosionAnimation(handler.getObjects().get(0).getX(), handler.getObjects().get(0).getY()));
+        }
+    }
+
+    public void render(Graphics g)
+    {
+        g.setColor(Color.gray);
+        g.fillRect(5, 5, 200 + bounds, 32);
+
+        g.setColor(Color.getHSBColor((1f * HEALTH) / 360, 1f, 1f));
+        g.fillRect(5, 5, HEALTH * 2, 32);
+
+        g.setColor(Color.white);
+        g.drawRect(5, 5, 200 + bounds, 32);
+
+        g.setFont(new Font("serif", Font.BOLD, 15));
+        g.drawString("Score: " + score, 5, 55);
+
+        g.drawString("Level: " + level, 5, 70);
+
+        g.drawString(Game.difficulty ? "Difficulty: HARD" : "Difficulty: NORMAL", 5, 85);
+
+        g.drawString("Space for shop", 5, 100);
+
+        g.setFont(new Font("serif", Font.BOLD, 50));
+        g.drawString(getLevel() % 10 == 0 ? "BOSS FIGHT!!" : "Level: " + level + "!", getLevel() % 10 == 0 ? Game.ANCHO / 2 - 180 : Game.ANCHO / 2 - 100, (int) y);
+
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void setScore(int score)
+    {
+        this.score = score;
+    }
+
+    public int getLevel()
+    {
+        return level;
+    }
+
+    public void setLevel(int level)
+    {
+        this.level = level;
+    }
+
+    public void setLevelAnimation(boolean levelAnimation)
+    {
+        LevelAnimation = levelAnimation;
+    }
+
 }
