4e1e85881c4984fd17cd2672f99a8c0e83a79213
HikingCarrot7 <ricardoibarra2044@gmail.com>
2019-07-26 19:38:23 -0500

    .

--- BattleShip/src/com/game/src/UI/PlacingShips.java
+++ BattleShip/src/com/game/src/UI/PlacingShips.java
@@ -354,13 +354,12 @@ public class PlacingShips implements Drawable, InputListener
 
         } else if (Main.GAMESTATE.equals(Main.STATE.ColocandoBarcos) && e.getButton() == MouseEvent.BUTTON1 && CONTBARCOSCOLOCADOS == 8 && r.intersects(continuar))
         {
-
-            cliente.setBarcos(cuadricula.obtenerTablero());
-
             if (!cliente.getServer())
             {
                 menu.crearCliente();
             }
+            
+            cliente.setBarcos(cuadricula.obtenerTablero());
 
             Main.GAMESTATE = Main.STATE.Jugando;
 
